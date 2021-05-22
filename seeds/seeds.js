420936223a4b5187dfd989f8d46e0817e0c78ed2
momogit <ferguson.tyra@gmail.com>
2021-05-22 18:50:56 -0400

    seeds completed

---
+++ seeds/seeds.js
@@ -0,0 +1,28 @@
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
+
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Post.bulkCreate(postData, {
+    returning: true,
+  });
+
+  await Comment.bulkCreate(commentData, {
+    returning: true,
+  });
+
+  process.exit(0);
+};
+
+seedDatabase();
