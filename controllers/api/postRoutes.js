3e7be95a3294388b214803175458d3a42b487217
momogit <ferguson.tyra@gmail.com>
2021-06-10 09:54:19 -0400

    fixing routes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,9 +1,78 @@
 const router = require('express').Router();
-const { Post } = require('../../models');
-// const withAuth = require('../../utils/auth');
+const { Post, User, Comment } = require('../../models');
 
+// get all users
+router.get('/', (req, res) => {
+    console.log('======================');
+    Post.findAll({
+        attributes: [
+            'id',
+            'title'
+        ],
+      include: [
+        // Comment model here -- attached username to comment
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username', 'twitter', 'github']
+        },
+      ]
+    })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
-router.post('/', async (req, res) => {
+  router.get('/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'title',
+        'created_at',
+        'post_content'
+      ],
+      include: [
+        // include the Comment model here:
+        {
+          model: User,
+          attributes: ['username', 'twitter', 'github']
+        },
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username', 'twitter', 'github']
+          }
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+router.post('/create', async (req, res) => {
     try {
         const newPost = await Post.create({
             ...req.body,
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -28,7 +28,7 @@ router.get('/', async (req, res) => {
 });
 
 
-router.get('/post/:id', withAuth, async (req,res) => {
+router.get('/posts/:id', withAuth, async (req,res) => {
     try {
         const postData = await Post.findByPk(req.params.id, {
             include: [
